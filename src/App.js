64bcb179ca8066f968fb374c3e8539ce5dd6dcfc
liketristan <128777159+liketristan@users.noreply.github.com>
2023-03-24 14:36:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello World{value}</div>;
+  const value = 'Ghost';
+  return <div>Hello{value}</div>;
 }
 
 export default App;
